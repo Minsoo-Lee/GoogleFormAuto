95201271d01ff115ee09eb5add782f2f9ebfdda1
이민수 <minsoo.james1101@gmail.com>
2025-08-01 18:46:14 +0900

    auth added

--- GoogleFormAuto/main.py
+++ GoogleFormAuto/main.py
@@ -1,16 +1,25 @@
 import wx
+
+from auth.auth_window import AuthDialog
 from window import main_frame
+
+def open_auth_dialog():
+    auth_dialog = AuthDialog()
+    auth_dialog.ShowModal()
+    result = auth_dialog.auth_success
+    auth_dialog.Destroy()
+    return result
 
 if __name__ == "__main__":
     app = wx.App(False)
-    frame = main_frame.MainFrame()
-    frame.Show()
-    app.MainLoop()
+    # frame = main_frame.MainFrame()
+    # frame.Show()
+    # app.MainLoop()
 
-    # if open_auth_dialog():
-    #     print("인증 성공. 메인 실행")
-    #     frame = window.MainFrame()
-    #     frame.Show()
-    #     app.MainLoop()
-    # else:
-    #     print("인증 실패. 종료합니다.")
+    if open_auth_dialog():
+        print("인증 성공. 메인 실행")
+        frame = main_frame.MainFrame()
+        frame.Show()
+        app.MainLoop()
+    else:
+        print("인증 실패. 종료합니다.")
