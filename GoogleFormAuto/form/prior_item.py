3153427411d1801db890ffcc7d77202103a07806
이민수 <minsoo.james1101@gmail.com>
2025-08-04 02:42:35 +0900

    prior list debugging

--- GoogleFormAuto/form/prior_item.py
+++ GoogleFormAuto/form/prior_item.py
@@ -61,6 +61,7 @@ class PriorItem:
 
     def get_prior_result_radio(self, item):
         result = [idx for idx, rank in enumerate(item[1]) for _ in range(int(rank))]
+        print(result)
         self.prior_result.append([AnswerType.RADIO, random.choice(result)])
 
     def get_prior_result_radio2(self, item):
